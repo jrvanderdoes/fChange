fcfed9b1555342d2a38251c9d2658b1aa0f16228
Jeremy <jeremyvanderdoes@gmail.com>
2023-07-22 22:30:59 -0400

    Update autoElbow_Method and documentation

--- R/autoElbow_Method.R
+++ R/autoElbow_Method.R
@@ -1,48 +1,34 @@
 
 #' Auto Elbow Method
 #'
-#' Method to determine the number of change points using the elbow method. Note,
-#'     cascading change points are not considered to allow for every possible
-#'     number of change points to be selectable.
+#' Method to determine the number of change points using the elbow method.
+#'  Change points are selected based on the max \eqn{M_n} statistic (see
+#'  `compute_Mn()`) in sub-segments, selecting the one that maximized total
+#'  variance explained.
 #'
 #' @param data Numeric data.frame with rows for evaluated values and columns
 #'    indicating FD
-#' @param test_stat_method XXXXX
 #' @param trim_function_method Function taking data as an argument and returning a
 #'     numeric value indicating how much should be trimmed on each end
 #' @param newVarExplain XXXXXXXXX
 #' @param maxCP XXXXXXXXX
 #' @param errorType String of 'L2' or 'Tr' indicating the error function to use
 #' @param demean XXXXXXXXX
-#' @param ... Additional parameters to pass into the respective functions
+#' @param ... Additional parameters to pass into the `trim_function_method()` or
+#' `compute_Mn()`.
 #'
 #' @return list with element 1 the data frame with the change point location and
 #'     element a ggplot of variance as a function of CPs
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' data_KL <- generate_data_fd(ns = c(100,50,100),
-#'                   eigsList = list(c(3,2,1,0.5),
-#'                                   c(3,2,1,0.5),
-#'                                   c(3,2,1,0.5)),
-#'                   basesList = list(
-#'                      fda::create.bspline.basis(nbasis=4, norder=4),
-#'                      fda::create.bspline.basis(nbasis=4, norder=4),
-#'                      fda::create.bspline.basis(nbasis=4, norder=4)),
-#'                   meansList = c(-1,1,-1),
-#'                   distsArray = c('Normal','Normal','Normal'),
-#'                   evals = seq(0,1,0.05),
-#'                   kappasArray = c(0, 0, 0))
-#'
-#' results <- .autoElbow_method(data=data_KL,
-#'                               test_stat_method=compute_Tn)
-#' }
+#' results <- .autoElbow_method(data=electricity)
 .autoElbow_method <- function(data,
-                              test_stat_method=compute_Mn,
                               trim_function_method=trim_function,
                               newVarExplain = 0.05, maxCP = 10,
                               errorType = 'L2', demean=TRUE, ... ){
+  # Params that could be entered, but only one option for now
+  test_stat_method <- compute_Mn
   # Setup
   n <- ncol(data)
   return_data <- data.frame('CP'=NA,
@@ -55,18 +41,17 @@
   nEnd <- ncol(as.data.frame(data))-trim_amt
   if(nStart> nEnd) return()
 
-  cat(paste0('Find CP (iters): 1'))
 
   # Run First
-  test_stat <- rep(NA, n)
-  for(k in nStart:nEnd){
-    test_stat[k] <- test_stat_method(data, k, ...)
-  }
-  return_data[1,] <- c(which.max(test_stat),
-                       .compute_total_var(data, which.max(test_stat),
-                                          errorType),1)
+  cat(paste0('Find CP (iters): 1'))
+
+  test_stats <- test_stat_method(data, ...)$allValues
+  loc <- which.max(test_stats[nStart:nEnd])+nStart-1
+
+  return_data[1,] <- c(loc, .compute_total_var(data,loc, errorType), 1)
 
   # Search until max CP or below varChange
+  #   Non-recursive as no absolute cutoff and only want to certain number
   i <- 1
   # Change second or redefine base change value
   while(nrow(return_data)<=min(n-1,maxCP) &&
@@ -86,39 +71,26 @@
 
     # Previous
     prev2currCP <- (CPs[prev_CP_loc-1]+1):(CPs[prev_CP_loc])
-    trim_amt <- trim_function_method(data[,prev2currCP], ...)
-    for(k in prev2currCP){
-      if(k < min(prev2currCP)+trim_amt ||
-         k > max(prev2currCP)-trim_amt){
-        test_stat[k] <- NA
-      }
-      else{
-        test_stat[k] <- test_stat_method(
-          as.data.frame(data[, prev2currCP]),
-          k-min(prev2currCP)+1, ...)
-      }
-    }
+    trim_amt <- trim_function_method(data[,prev2currCP])#, ...)
+    test_stats[prev2currCP] <- test_stat_method(data[,prev2currCP])$allValues#, ...)$allValues
+    # Make NA if range too small (trimmed)
+    test_stats[c(min(prev2currCP) + 1:trim_amt - 1,
+                 max(prev2currCP) - 1:trim_amt + 1)] <- NA
+
     # Next
     curr2nextCP <- (CPs[prev_CP_loc]+1):CPs[prev_CP_loc+1]
-    trim_amt <- trim_function_method(data[,curr2nextCP], ...)
-    for(k in curr2nextCP){
-      if(k < min(curr2nextCP)+trim_amt ||
-         k > max(curr2nextCP)-trim_amt){
-        test_stat[k] <- NA
-      }
-      else{
-        test_stat[k] <- test_stat_method(
-          as.data.frame(data[, curr2nextCP]),
-          k-min(curr2nextCP)+1, ...)
-      }
-    }
+    trim_amt <- trim_function_method(data[,curr2nextCP])#, ...)
+    test_stats[curr2nextCP] <- test_stat_method(data[,curr2nextCP])$allValues#, ...)$allValues
+    # Make NA if range too small (trimmed)
+    test_stats[c(min(curr2nextCP) + 1:trim_amt - 1,
+                 max(curr2nextCP) - 1:trim_amt + 1)] <- NA
 
     # If there is no possibilities after trimming
-    if(sum(is.na(test_stat))==n)
-      break
+    if(sum(is.na(test_stats))==n) break
 
-    ## Get total variance for each potential CP
-    data_segments <- .split_on_NA(test_stat)
+    ## Get total variance for each potential CP on the segments
+    #   Can do less repeating here eventually
+    data_segments <- .split_on_NA(test_stats)
 
     return_data_tmp <- data.frame('CP'=NA,'Var'=NA)
     for(k in 1:length(data_segments)){
@@ -126,7 +98,7 @@
       value_max <- max(data_segments[[k]])
 
       # Get CP and total variance with full data
-      section_max <- which(test_stat==value_max)
+      section_max <- which(test_stats==value_max)
       tmp <- c(section_max, return_data$CP)
       tmp <- tmp[order(tmp)]
 
@@ -147,15 +119,17 @@
               'data_demean'=.demean(data,return_data$CP)))
 }
 
-#' Title
+#' Demean Data
 #'
-#' @param data XXXXX
-#' @param CPs XXXXX
+#' This (internal) function de-means the data based on some given change points.
 #'
-#' @return XXXXX
+#' @inheritParams .autoElbow_method
+#' @param CPs Vector of numerics (integers) indicating the location of the
+#'  changes in the data.
 #'
-#' @examples
-#' # XXXXX
+#' @return Data.frame of the form given in data, but with the means removed.
+#'
+#' @noRd
 .demean <- function(data, CPs){
   CPs <- c(0,CPs[order(CPs)], ncol(data))
   data_demean <- data
