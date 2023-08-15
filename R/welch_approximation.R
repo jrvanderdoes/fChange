87a12d8fc783e2e9994d81755e0f8f941682d5fa
Jeremy <jeremyvanderdoes@gmail.com>
2023-08-15 12:03:50 -0400

    Update Welch Code (save some temp old files)

--- R/welch_approximation.R
+++ R/welch_approximation.R
@@ -25,117 +25,38 @@
 #' @examples
 #' # Note, TVal generally bigger than 1.
 #' welch_approximation(electricity, TVal = 1, h = 0)
-welch_approximation <- function(X, alpha = 0.05, TVal = length(X[1, ]),
+welch_approximation <- function(X, alpha = 0.05, TVal = ncol(X),
                                 W = NULL, W1 = NULL, M = 100,
                                 h = TVal^(1 / 3), K = bartlett_kernel, ...) {
   if (is.null(W)) {
     W <- computeSpaceMeasuringVectors(M, "BM", X)
   }
-  # if(is.null(W)){
-  #   W <- as.data.frame(sapply(rep(0,M),sde::BM, N=length(X[,1])-1))
-  # }else{
-  #   W <- as.data.frame(W)
-  # }
-
   if (is.null(W1)) {
     W1 <- computeSpaceMeasuringVectors(M, "BM", X)
   }
 
-  muVals <- rep(0, M)
-  sigma2Vals <- rep(0, M)
+  muVals <- sigma2Vals <- rep(0, M)
 
   for (i in 1:M) {
     v <- W[, i]
     v1 <- W1[, i]
 
-    CHat <- .estimateLRV(X = X, v1 = v, v2 = v, TVal = TVal, h = h, K = K)
-    CHat1 <- .estimateLRV(X = X, v1 = v, v2 = v1, TVal = TVal, h = h, K = K)
+    # CHat <- .estimateLRV(X = X, v1 = v, v2 = v, TVal = TVal, h = h, K = K)
+    # CHat1 <- .estimateLRV(X = X, v1 = v, v2 = v1, TVal = TVal, h = h, K = K)
+    CHat <- .computeLRVEstimate(X = X, v1 = v, v2 = v, TVal = TVal, h = h, K = K)
+    CHat1 <- .computeLRVEstimate(X = X, v1 = v, v2 = v1, TVal = TVal, h = h, K = K)
 
     muVals[i] <- CHat
     sigma2Vals[i] <- CHat1 * Conj(CHat1) # pairs
   }
 
   muHat <- 1 / 6 * mean(muVals)
-  sigma2Hat <- (1 / 90) * 2 * mean(sigma2Vals)
+  sigma2Hat <- (1 / 90) * 2 * mean(sigma2Vals)/length(sigma2Vals)
 
   betaHat <- Re(sigma2Hat / (2 * muHat))
   nuHat <- Re((2 * muHat^2) / sigma2Hat)
 
-  betaHat * stats::qchisq(1 - alpha, df = nuHat) / nrow(X)
-}
-
-
-#' Estimate Long Run Variance
-#'
-#' This (internal) function estimates CHat
-#'
-#' @param X Numeric data.frame with evaled points on rows and fd objects in columns
-#' @param v1 Vector of length nrow(X) indicating a random vector in the space
-#' @param v2 Vector of length nrow(X) indicating a random vector in the space
-#' @param TVal Numeric value indicate the number of FDs.
-#' @param h Numeric value indicating the amount of blocking.
-#' @param K Function for the Kernel.
-#'
-#' @return Numeric indicated estimated CHat value
-#'
-#' @noRd
-#'
-#' @examples
-#' # This is an internal function, see welch_approximation for usage.
-.estimateLRV <- function(X, v1, v2, TVal, h, K) {
-  eps1 <- exp(complex(real = 0, imaginary = 1) * (t(X) %*% v1))
-  # eps1Bar <- mean(eps1)#1/TVal * sum(eps1)
-
-  eps2 <- exp(complex(real = 0, imaginary = 1) * (t(X) %*% v2))
-  # eps2Bar <- mean(eps2)#1/TVal * sum(eps2)
-
-  gammaHat <- rep(NA, length((1 - TVal):(TVal - 1)))
-
-  for (l in (1 - TVal):(TVal - 1)) {
-    gammaHat[TVal + l] <- .getAutocov(
-      eps1 = eps1, eps2 = eps2,
-      # eps1Bar=eps1Bar, eps2Bar=eps2Bar,
-      l = l, TVal = TVal
-    )
-  }
-
-  .computeLRVEstimate(h = h, TVal = TVal, gammaHat = gammaHat, K = K)
-}
-
-
-#' Estimate Autocovariance Estimates
-#'
-#' This (internal) function estimates autocovariance at single point
-#'
-#' @param eps1 Complex vector indicating exp(i, <X,v>)
-#' @param eps2 Complex vector indicating exp(i, <X,v2>)
-#' @param l Integer indicating lags
-#' @param TVal Numeric value indicate the number of FDs.
-#'
-#' @return Numeric indicating autocovariance value at a particular lag
-#'
-#' @noRd
-#'
-#' @examples
-#' # This is an internal function, see .estimateLRV for usage.
-.getAutocov <- function(eps1, eps2, l, TVal) { # eps1Bar, eps2Bar, l, TVal){
-  gammaVal <- 0
-  eps1Bar <- mean(eps1)
-  eps2Bar <- mean(eps2)
-
-  if (l >= 0) {
-    for (j in (1 + l):TVal) {
-      gammaVal <- gammaVal +
-        (eps1[j - l] - eps1Bar) * Conj(eps2[j] - eps2Bar)
-    }
-  } else {
-    for (j in (1 - l):TVal) {
-      gammaVal <- gammaVal +
-        (eps1[j + l] - eps1Bar) * Conj(eps2[j] - eps2Bar) # Confirm this j+l swap
-    }
-  }
-
-  1 / TVal * gammaVal
+  betaHat * stats::qchisq(1 - alpha, df = nuHat) #/ nrow(X)
 }
 
 
@@ -151,15 +72,51 @@ welch_approximation <- function(X, alpha = 0.05, TVal = length(X[1, ]),
 #' @return Numeric estimating LRV value
 #'
 #' @noRd
-#'
-#' @examples
-#' # This is an internal function, see .estimateLRV for usage.
-.computeLRVEstimate <- function(h, TVal, gammaHat, K) {
-  CVal <- 0
+.computeLRVEstimate <- function(X, v1, v2, TVal, h, K){
+  iters <- (1 - TVal):(TVal - 1)
 
-  for (l in (1 - TVal):(TVal - 1)) {
-    CVal <- CVal + K(l, h) * gammaHat[TVal + l]
+  # Move so pass in function values to avoid re-computation later
+  eps1 <- exp(1/nrow(X) * complex(imaginary = 1) * (t(X) %*% v1) )
+  eps2 <- exp(1/nrow(X) * complex(imaginary = 1) * (t(X) %*% v2) )
+
+  values <- sapply(iters, function(k, K, h, X1, TVal, eps1, eps2, mean1, mean2) {
+    Kval <- K(k, h)
+
+    ifelse(Kval == 0,
+           0,
+           Kval * .estimWelchGamma(
+             k = k, X = X1, TVal=TVal,
+             eps1 = eps1, eps2 = eps2,
+             mean1 = mean1, mean2 = mean2
+           )
+    )
+  },
+  K = K, h = h, X1 = X, TVal=TVal, eps1 = eps1, eps2 = eps2,
+  mean1 = mean(eps1), mean2 = mean(eps2)
+  )
+
+  sum(values)
+}
+
+
+#' Estimate Autocovariance Estimates
+#'
+#' This (internal) function estimates autocovariance at single point
+#'
+#' @param eps1 Complex vector indicating exp(i, <X,v>)
+#' @param eps2 Complex vector indicating exp(i, <X,v2>)
+#' @param l Integer indicating lags
+#' @param TVal Numeric value indicate the number of FDs.
+#'
+#' @return Numeric indicating autocovariance value at a particular lag
+#'
+#' @noRd
+.estimWelchGamma <- function(k, X, TVal,  eps1, eps2, mean1, mean2) {
+  if (k >= 0) {
+    rs <- (k+1):TVal
+  } else {
+    rs <- (1-k):TVal
   }
 
-  CVal
+  1/(TVal) * sum( (eps1[rs - k] - mean1) * Conj(eps2[rs]-mean2) )
 }
