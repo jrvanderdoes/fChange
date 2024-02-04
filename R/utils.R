1401ff859f2b95d92be57e5980487889a3419313
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-02-04 02:00:19 -0500

    Remove approx integral function

--- R/utils.R
+++ R/utils.R
@@ -1,32 +1,32 @@
-#' Approximate Integral
+#' #' Approximate Integral
+#' #'
+#' #' This (internal) function computes the estimates the integral for
+#' #'  evenly spaced observations.
+#' #'
+#' #' @param y Numeric vector of data
+#' #' @param type (Optional) String indicating type of integration. Currently the
+#' #'  options are "Rectangle" and "Trapezoidal", both done using Riemann sums.
+#' #'  Default is "Trapezoidal".
+#' #'
+#' #' @return Numeric indicating the estimated integral of the curve
+#' #'
+#' #' @noRd
+#' #'
+#' #' @examples
+#' #' .approx_int(rep(1, 10), type = "Rectangle")
+#' #' .approx_int(seq(0, 1, length.out = 20))
+#' #' .approx_int(seq(0, 1, length.out = 20)^2)
+#' .approx_int <- function(y, type = "Trapezoidal") {
+#'   if (type == "Rectangle") { # Rect Approx
+#'     value <- sum(y) / length(y)
+#'   } else if (type == "Trapezoidal") { # Trap Approx
+#'     value <- sum(y[-1] + y[-length(y)]) / (2 * (length(y) - 1))
+#'   } else {
+#'     stop("Error: This type is not yet implemented. See documentation.")
+#'   }
 #'
-#' This (internal) function computes the estimates the integral for
-#'  evenly spaced observations.
-#'
-#' @param y Numeric vector of data
-#' @param type (Optional) String indicating type of integration. Currently the
-#'  options are "Rectangle" and "Trapezoidal", both done using Riemann sums.
-#'  Default is "Trapezoidal".
-#'
-#' @return Numeric indicating the estimated integral of the curve
-#'
-#' @noRd
-#'
-#' @examples
-#' .approx_int(rep(1, 10), type = "Rectangle")
-#' .approx_int(seq(0, 1, length.out = 20))
-#' .approx_int(seq(0, 1, length.out = 20)^2)
-.approx_int <- function(y, type = "Trapezoidal") {
-  if (type == "Rectangle") { # Rect Approx
-    value <- sum(y) / length(y)
-  } else if (type == "Trapezoidal") { # Trap Approx
-    value <- sum(y[-1] + y[-length(y)]) / (2 * (length(y) - 1))
-  } else {
-    stop("Error: This type is not yet implemented. See documentation.")
-  }
-
-  value
-}
+#'   value
+#' }
 
 
 #' Specify Decimal
