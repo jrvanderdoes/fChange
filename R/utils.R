8db325d5987a32bd3bd590d6de7ad68eafab3de0
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-02-10 02:27:12 -0500

    Add few functions to utils from other functions

--- R/utils.R
+++ R/utils.R
@@ -59,3 +59,62 @@
 .select_n <- function(vals,n){
   vals[round(seq(1,length(vals),length.out=n))]
 }
+
+
+#' Get Chunks
+#'
+#' This (internal) function splits the vector x into a chunksN number of
+#'     subsegments. The values of x are kept in order (i.e. no scrambled).
+#'
+#' @param x Vector of values
+#' @param chunksN Numeric indicating the number of chunks to split X into
+#'
+#' @return A list with chunksN items, each containing an similiar sized subset
+#'     of the original vector
+#'
+#' @noRd
+#'
+#' @examples
+#' .getChunks(1:100, 1)
+#' .getChunks(1:100, 2)
+#' .getChunks(1:100, 5)
+.getChunks <- function(x, chunksN) {
+  if (chunksN < 2) {
+    return(x)
+  }
+  split(x, cut(x, chunksN, labels = FALSE))
+}
+
+
+#' Convert List of Samples into a Data Frame
+#'
+#' This (internal) function takes a list with differ length data.frames or
+#'     vectors and pads them all to make a clean data.frame.
+#'
+#' @param data_list List of elements to be combined to a data.frame.
+#'
+#' @return Data.frame of the data in data_list
+#'
+#' @noRd
+#'
+#' @examples
+#' # This is an internal function and will not be viewable to user. See
+#' #     generalized_resampling.
+.convertSamplesToDF <- function(data_list) {
+  m <- length(data_list)
+  maxLen <- 0
+  for (ii in 1:m) {
+    maxLen <- max(maxLen, length(data_list[[ii]]))
+  }
+
+  data_df <- data.frame(matrix(nrow = maxLen, ncol = m))
+
+  for (ii in 1:length(data_list)) {
+    data_df[, ii] <- c(
+      data_list[[ii]],
+      rep(NA, maxLen - length(data_list[[ii]]))
+    )
+  }
+
+  data_df
+}
