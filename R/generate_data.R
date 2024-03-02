a0403979423598fbd860d0e0a1c916da6405caa5
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-03-02 00:32:15 -0500

    Update data generation

--- R/generate_data.R
+++ R/generate_data.R
@@ -8,20 +8,27 @@
 #' @export
 #'
 #' @examples
-generateOU <- function(resolution, N){
+generateOU <- function(resolution, N, burnin=500, rho=0){
 
   times <- 1:resolution/resolution
 
   # Covariance structure (OU process Cov)
   comat <- matrix(NA,resolution,resolution)
   for (i in 1:resolution){
-    comat[i,] <- exp(-times[i]/2-times/2) * pmin(exp(times[i]), exp(times))
+    #comat[i,] <- exp(-times[i]/2-times/2) * pmin(exp(times[i]), exp(times))
+    comat[i,] <- exp(-abs(times[i]-times)/2)
   }
 
-  fiid <- MASS::mvrnorm(n = N,
+  fiid <- MASS::mvrnorm(n = N+burnin,
                         mu = c(rep(0,resolution)),
                         Sigma = comat,
                         empirical = TRUE)
 
-  t(fiid)
+  data <- data.frame(matrix(0,ncol = nrow(fiid),nrow=ncol(fiid)))
+  data[,1] <- fiid[1,]
+  for(i in 2:ncol(data)){
+    data[,i] <- rho*data[,i-1] + fiid[i,]
+  }
+
+  data
 }
