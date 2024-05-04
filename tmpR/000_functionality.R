c0c36c75517e268592f9af188b839eb572cb987d
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-05-03 21:24:18 -0400

    Add temp file

---
+++ tmpR/000_functionality.R
@@ -0,0 +1,32 @@
+########################
+##    Functionality
+########################
+
+######
+##  FTSA
+https://cran.r-project.org/web/packages/ftsa/ftsa.pdf
+
+######
+## ACF
+https://github.com/GMestreM/fdaACF/tree/master
+
+######
+## WHITE NOISE
+https://cran.r-project.org/web/packages/wwntests/index.html
+file:///C:/Users/jerem/Downloads/23-SS143.pdf
+
+
+######
+## CLASSES
+https://www.datamentor.io/r-programming/s4-class
+https://stackoverflow.com/questions/9266194/r-function-overloading
+https://stat.ethz.ch/R-manual/R-devel/library/methods/html/Methods_for_S3.html#:~:text=S3%20is%20the%20original%2C%20simpler%20for%20initial%20programming,R%2C%20the%20two%20versions%20attempt%20to%20work%20together.
+http://adv-r.had.co.nz/S3.html
+http://adv-r.had.co.nz/S4.html
+https://www.datacamp.com/tutorial/r-objects-and-classes
+https://stackoverflow.com/questions/14604269/how-to-create-a-dataframe-of-user-defined-s4-classes-in-r
+https://www.programiz.com/r/objects-classes
+https://rstudio-education.github.io/hopr/s3.html
+
+######
+## XXXXXXX
