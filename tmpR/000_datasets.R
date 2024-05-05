9e1980d3e607f951e4de609338b3c79bad68557d
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-05-04 21:24:24 -0400

    Add temp file

---
+++ tmpR/000_datasets.R
@@ -0,0 +1,37 @@
+########################
+##    DATA SETS
+########################
+
+########
+## Earthquake Detection
+
+install.packages('eseis')
+library(eseis)
+eseis::aux_getFDSNdata()
+
+########
+## PACKAGE OF FDATA
+
+https://cran.r-project.org/web/packages/fds/fds.pdf
+
+########
+## Mortality
+
+https://www.mortality.org/Country/Country?cntr=FRATNP
+https://cran.r-project.org/web/packages/demography/demography.pdf
+
+
+########
+## AU Temp
+
+http://www.bom.gov.au/
+
+
+########
+## Backtest data
+
+https://www.backtestmarket.com/
+
+
+########
+## XXXXXXX
