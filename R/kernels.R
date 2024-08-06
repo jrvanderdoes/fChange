b95ebc2d6be3551146a1512e9fb6e0e1ae5283a1
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-08-06 14:00:29 -0400

    Add flat-top kernel

--- R/kernels.R
+++ R/kernels.R
@@ -14,3 +14,22 @@
 bartlett_kernel <- function(l, h) {
   pmax(0,ifelse(is.nan(1 - abs(l) / h),1,1 - abs(l) / h))
 }
+
+
+#' Title
+#'
+#' @inheritParams bartlett_kernel
+#'
+#' @return A numeric value indicating value of Kernel at that point
+#' @export
+#'
+#' @references
+#' L. Horvath, P. Kokoszka, G. Rice (2014) "Testing stationarity of functional time series", Journal of
+#'  Econometrics, 179(1), 66-82.
+#'
+#' @examples
+#' flat_kernel(1, 2)
+#' flat_kernel(0, 0)
+flat_kernel = function(l,h) {
+  pmin(1, pmax(1.1 - abs(x/h), 0))
+}
