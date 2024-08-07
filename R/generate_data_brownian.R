3834c0f7c55ab1876c73a9c5871a73a9cf73b190
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-08-07 02:41:21 -0400

    Update generation of data

---
+++ R/generate_data_brownian.R
@@ -0,0 +1,64 @@
+#' Generate a Brownian Motion Process
+#'
+#' Generate a functional time series according to an iid Brownian Motion process.
+#'  Each observation is discretized in the points indicated in \code{v}.
+#'
+#' @param N Numeric. The number of observations for the generated data.
+#' @param v Numeric. Discretization points of the curves. By default it is
+#'  evenly spaced on [0,1], i.e. \code{seq(from = 0, to = 1, length.out = 100)}.
+#' @param sd Numeric. Standard deviation of the Brownian Motion process.
+#'  The default is \code{1}.
+#'
+#' @return Functional time series object
+#' @export
+#'
+#' @examples
+#' N <- 100
+#' v <- seq(from = 0, to = 1, length.out = 20)
+#' bmotion <- generate_brownian_motion(N, v, sd = 1)
+generate_brownian_motion <- function(
+    N, v = seq(from = 0, to = 1, length.out = 100), sd = 1){
+
+  res <- length(v)
+
+  data <- matrix(0, ncol = res, nrow = N)
+  data[,2:res] <- t(apply(matrix(stats::rnorm( (res-1) *N, sd = sd),
+                                     ncol = res-1, nrow = N), 1, cumsum))
+
+  funts(X=data,intraobs = v)
+}
+
+
+#' Generate a Brownian Bridge Process
+#'
+#' Generate a functional time series from an iid Brownian Bridge process.
+#'  If \eqn{W(t)} is a Wiener process, the Brownian Bridge is
+#'  defined as \eqn{W(t) - tW(1)}.
+#'  Each functional observation is discretized in the points
+#'  indicated in \code{v}.
+#'
+#' @inheritParams generate_brownian_motion
+#'
+#' @return Functional time series object
+#' @export
+#'
+#' @examples
+#' N <- 100
+#' v <- seq(from = 0, to = 1, length.out = 20)
+#' bbridge <- generate_brownian_bridge(N, v, sd=1)
+generate_brownian_bridge <- function(
+    N, v = seq(from = 0, to = 1, length.out = 100), sd = 1){
+
+  res <- length(v)
+
+  # Initialize
+  data <- matrix(0, ncol = res, nrow = N)
+  data[,2:res] <-
+    as.matrix(generate_brownian_motion(N, v = v[1:res-1], sd)$data)
+  data <- data -
+    data[,res] * matrix( rep(v, times = N),
+                             ncol = res, nrow = N, byrow = T)
+
+  funts(X=data,intraobs = v)
+}
+
