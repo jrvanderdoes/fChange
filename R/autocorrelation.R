d3094ac4f163363340ccf9359d29129b19bacc17
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-08-14 19:49:13 -0400

    Add autocorrelation and autocovariance to package

--- R/autocorrelation.R
+++ R/autocorrelation.R
@@ -1,34 +1,24 @@
 #' Estimate the autocorrelation function of the series
 #'
-#' @description Obtain the empirical autocorrelation function for
-#' lags \eqn{= 0,...,}\code{nlags} of the functional time
-#' series. Given \eqn{Y_{1},...,Y_{T}} a functional time
-#' series, the sample autocovariance functions
-#' \eqn{\hat{C}_{h}(u,v)} are given by:
-#' \deqn{\hat{C}_{h}(u,v) =  \frac{1}{T} \sum_{i=1}^{T-h}(Y_{i}(u) - \overline{Y}_{T}(u))(Y_{i+h}(v) - \overline{Y}_{T}(v))}
-#' where
-#' \eqn{ \overline{Y}_{T}(u) = \frac{1}{T} \sum_{i = 1}^{T} Y_{i}(t)}
-#' denotes the sample mean function. By normalizing these
-#' functions using the normalizing factor
-#' \eqn{\int\hat{C}_{0}(u,u)du}, the range of the
-#' autocovariance functions becomes \eqn{(0,1)}; thus
-#' defining the autocorrelation functions of the series
+#' Obtain the empirical autocorrelation function for lags
+#'  \eqn{= 0,...,}\code{nlags} of the functional time series. Given
+#'  \eqn{Y_{1},...,Y_{T}} a functional time series, the sample autocovariance
+#'  functions \eqn{\hat{C}_{h}(u,v)} are given by:
+#'  \deqn{\hat{C}_{h}(u,v) =  \frac{1}{T} \sum_{i=1}^{T-h}(Y_{i}(u) - \overline{X}_{T}(u))(Y_{i+h}(v) - \overline{X}_{T}(v))}
+#'  where
+#'  \eqn{ \overline{X}_{T}(u) = \frac{1}{T} \sum_{i = 1}^{T} Y_{i}(t)}
+#'  denotes the sample mean function. By normalizing these functions using
+#'  the normalizing factor \eqn{\int\hat{C}_{0}(u,u)du}, the range of the
+#'  autocovariance functions becomes \eqn{(0,1)}; thus defining the
+#'  autocorrelation functions of the series.
 #'
-#' @param Y Matrix containing the discretized values
-#' of the functional time series. The dimension of the
-#' matrix is \eqn{(n x m)}, where \eqn{n} is the
-#' number of curves and \eqn{m} is the number of points
-#' observed in each curve.
-#' @param v Discretization points of the curves, by default
-#' \code{seq(from = 0, to = 1, length.out = 100)}.
-#' @param nlags Number of lagged covariance operators
-#' of the functional time series that will be used
-#' to estimate the autocorrelation function.
+#' @param X funts object
+#' @param nlags Number of lagged covariance operators of the functional
+#'  time series that will be used to estimate the autocorrelation function.
 #'
-#' @return Return a list with the lagged autocorrelation
-#' functions estimated from the data. Each function is given
-#' by a \eqn{(m x m)} matrix, where \eqn{m} is the
-#' number of points observed in each curve.
+#' @return Return a list with the lagged autocorrelation functions estimated
+#'  from the data. Each function is given by a \eqn{(m x m)} matrix, where
+#'  \eqn{m} is the number of points observed in each curve.
 #' @export
 #'
 #' @examples
@@ -39,7 +29,7 @@
 #' sig <- 2
 #' bbridge <- generate_brownian_bridge(N, v, sig)
 #' nlags <- 1
-#' lagged_autocor <- obtain_autocorrelation(Y = bbridge,
+#' lagged_autocor <- obtain_autocorrelation(X = bbridge,
 #'                                         nlags = nlags)
 #' image(x = v, y = v, z = lagged_autocor$Lag0)
 #'
@@ -49,12 +39,11 @@
 #' N <- 500
 #' v <- seq(from = 0, to = 1, length.out = 50)
 #' sig <- 2
-#' bbridge <- simulate_iid_brownian_bridge(N, v, sig)
+#' bbridge <- generate_brownian_bridge(N, v, sig)
 #' nlags <- 4
-#' lagged_autocov <- obtain_autocovariance(Y = bbridge,
+#' lagged_autocov <- obtain_autocovariance(X = bbridge,
 #'                                         nlags = nlags)
-#' lagged_autocor <- obtain_autocorrelation(Y = bbridge,
-#'                                          v = v,
+#' lagged_autocor <- obtain_autocorrelation(X = bbridge,
 #'                                          nlags = nlags)
 #'
 #' opar <- par(no.readonly = TRUE)
@@ -82,10 +71,13 @@
 #'            main = "Autocorrelation")
 #' par(opar)
 #' }
-obtain_autocorrelation <- function(Y, nlags){
+obtain_autocorrelation <- function(X, nlags){
+  X <- .check_data(X)
 
-  fun.autocovariance <- .compute_autocovariance(Y,nlags)
-  normalization.value <- dot_integrate_uneven(r = Y$intraobs, v = diag(fun.autocovariance$Lag0))
+  fun.autocovariance <- .compute_autocovariance(X,nlags)
+  normalization.value <-
+    dot_integrate_uneven(r = X$intraobs, v = diag(fun.autocovariance$Lag0))
+
   fun.autocorrelation <- list()
   for(kk in 0:nlags){
     fun.autocorrelation[[paste("Lag",kk,sep = "")]] <-
@@ -96,23 +88,19 @@ obtain_autocorrelation <- function(Y, nlags){
 }
 
 
-
 #' Generate a 3D plot of the autocovariance surface of a given FTS
 #'
-#' @description Obtain a 3D plot of the autocovariance surfaces of a
-#' given functional time series. This visualization is
-#' useful to detect any kind of dependency between
-#' the discretization points of the series.
+#' Obtain a 3D plot of the autocovariance surfaces of a given functional
+#'  time series. This visualization is useful to detect any kind of dependency
+#'  between the discretization points of the series.
 #'
-#' @param fun.autocovariance A list obtained by
-#' calling the function \code{obtain_autocovariance}.
-#' @param lag An integer between 0 and \code{nlags}, indicating
-#' the lagged autocovariance function to be plotted.
-#' By default 0.
-#' @param ... Further arguments passed to the  \code{persp}
-#' function.
+#' @param fun.autocovariance A list obtained by calling the function
+#'  \code{obtain_autocovariance}.
+#' @param lag An integer between 0 and \code{nlags}, indicating the lagged
+#'  autocovariance function to be plotted. By default 0.
+#' @param ... Further arguments passed to the  \code{persp} function.
 #'
-#' @return XX
+#' @return Graphic from \code{persp}
 #' @export
 #'
 #' @examples
@@ -134,7 +122,7 @@ obtain_autocorrelation <- function(Y, nlags){
 #' sig <- 2
 #' bbridge <- simulate_iid_brownian_bridge(N, v, sig)
 #' nlags <- 4
-#' lagged_autocov <- obtain_autocovariance(Y = bbridge,nlags = nlags)
+#' lagged_autocov <- obtain_autocovariance(X = bbridge,nlags = nlags)
 #' opar <- par(no.readonly = TRUE)
 #' par(mfrow = c(1,5))
 #' for(k in 0:nlags){
@@ -170,3 +158,81 @@ plot_autocovariance <- function(fun.autocovariance, lag = 0, ...){
   do.call(graphics::persp,arguments)
 }
 
+
+
+#' Estimate the autocovariance function of the series
+#'
+#' Obtain the empirical autocovariance function for lags
+#'   \eqn{= 0,...,}\code{nlags} of the functional time series. Given
+#'   \eqn{Y_{1},...,Y_{T}} a functional time series, the sample autocovariance
+#'   functions \eqn{\hat{C}_{h}(u,v)} are given by:
+#'   \deqn{\hat{C}_{h}(u,v) =  \frac{1}{T} \sum_{i=1}^{T-h}(Y_{i}(u) - \overline{Y}_{T}(u))(Y_{i+h}(v) - \overline{Y}_{T}(v))}
+#'   where
+#'   \eqn{ \overline{Y}_{T}(u) = \frac{1}{T} \sum_{i = 1}^{T} Y_{i}(t)}
+#'   denotes the sample mean function.
+#'
+#' @param X funts object
+#' @param nlags Number of lagged covariance operators of the functional
+#'  time series that will be used to estimate the autocorrelation function.
+#'
+#' @return Return a list with the lagged autocovariance functions
+#'  estimated from the data. Each function is given by a \eqn{(m x m)}
+#'  matrix, where \eqn{m} is the number of points observed in each curve.
+#' @export
+#'
+#' @examples
+#' # Example 1
+#'
+#' N <- 100
+#' v <- seq(from = 0, to = 1, length.out = 10)
+#' sig <- 2
+#' bbridge <- simulate_iid_brownian_bridge(N, v, sig)
+#' nlags <- 1
+#' lagged_autocov <- obtain_autocovariance(Y = bbridge,
+#'                                         nlags = nlags)
+#' image(x = v, y = v, z = lagged_autocov$Lag0)
+#'
+#' \donttest{
+#' # Example 2
+#'
+#' N <- 500
+#' v <- seq(from = 0, to = 1, length.out = 50)
+#' sig <- 2
+#' bbridge <- simulate_iid_brownian_bridge(N, v, sig)
+#' nlags <- 10
+#' lagged_autocov <- obtain_autocovariance(Y = bbridge,
+#'                                         nlags = nlags)
+#' image(x = v, y = v, z = lagged_autocov$Lag0)
+#' image(x = v, y = v, z = lagged_autocov$Lag10)
+#'
+#' # Example 3
+#'
+#' require(fields)
+#' N <- 500
+#' v <- seq(from = 0, to = 1, length.out = 50)
+#' sig <- 2
+#' bbridge <- simulate_iid_brownian_bridge(N, v, sig)
+#' nlags <- 4
+#' lagged_autocov <- obtain_autocovariance(Y = bbridge,
+#'                                         nlags = nlags)
+#' z_lims <- range(lagged_autocov$Lag0)
+#' colors <- heat.colors(12)
+#' opar <- par(no.readonly = TRUE)
+#' par(mfrow = c(1,5))
+#' par(oma=c( 0,0,0,6))
+#' for(k in 0:nlags){
+#'    image(x=v,
+#'          y=v,
+#'          z = lagged_autocov[[paste0("Lag",k)]],
+#'          main = paste("Lag",k),
+#'          col = colors,
+#'          xlab = "u",
+#'          ylab = "v")
+#' }
+#' par(oma=c( 0,0,0,2.5)) # reset margin to be much smaller.
+#' image.plot( legend.only=TRUE, legend.width = 2,zlim=z_lims, col = colors)
+#' par(opar)
+#' }
+obtain_autocovariance <- function(X, nlags){
+  .compute_autocovariance(X, nlags)
+}
