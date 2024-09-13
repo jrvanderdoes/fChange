a9dca796b4ca804a55a6fffc9c1234503ae8a4e8
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-09-13 00:04:45 -0400

    Add file for change wrapper

---
+++ R/change.R
@@ -0,0 +1,6 @@
+change <- function(X,
+                   method=c('characteristic','mean','robust','eigen','trace'),
+                   statistic=c('Tn','Mn'),
+                   critical=c('simulation','permutation','welch')){
+
+}
