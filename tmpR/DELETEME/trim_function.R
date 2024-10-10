32c0322d62dfe8d46634035d6697ae3690bbadc9
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-10-09 21:29:01 -0400

    Add temp files

---
+++ tmpR/DELETEME/trim_function.R
@@ -0,0 +1,22 @@
+#' Basic trim function
+#'
+#' Basic trim function to select how much data to consider.
+#'
+#' @param data Numeric data.frame with rows for evaluated values and columns
+#'    indicating FD
+#' @param minVal Integer indicating the minimal trim amount
+#' @param ... Unused, but exist to allow for use in other functions
+#'
+#' @return Integer value indicating the amount to trim
+#' @export
+#'
+#' @references Rice, G, Zhang, C. (2021), \emph{Consistency of binary
+#'     segmentation for multiple change-point estimation with functional data}
+#'     (https://www.sciencedirect.com/science/article/pii/S0167715221001905)
+#'
+#' @examples
+#' trim_function(electricity)
+trim_function <- function(data, minVal = 10, ...) {
+  # TODO: Change to funts
+  max(round(minVal), floor(log(ncol(as.data.frame(data)))), na.rm = TRUE)
+}
