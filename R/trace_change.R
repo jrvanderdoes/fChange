e41a4abdd6c565b70d6953f221d8664870844f1b
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-11-06 21:34:41 -0500

    Update change

--- R/trace_change.R
+++ R/trace_change.R
@@ -1,18 +1,19 @@
-
-
 #' Testing changes in the trace of the covariance operator in functional data
 #'
 #' This function tests and detects changes in the trace of the covariance operator.
 #'
+#' @details This function dates and detects changes in trace of the covariance
+#'  function. This can be interpreted as the changes in the total variation
+#'  of the the functional data. Trace is defined as the infinite sum of
+#'  the eigenvalues of the covariance operator and for the sake of
+#'  implementation purpose, the sum is truncated up to the total number
+#'  of basis functions that defines the functional data at hand. The critical
+#'  values are approximated via \code{M} Monte Carlo simulations.
 #'
-#' @param fdobj A functional data object of class '\code{fd}'
+#' @param X A functional data object of class '\code{fd}'
 #' @param CPs If not NULL then the data is centered considering the changes.
-#' @param delta Trimming parameter to estimate the covariance function using partial sum estimates.
 #' @param M Number of monte carlo simulations used to get the critical values. The default value is \code{M=1000}
 #'  value is \code{h=2}.
-#'
-#'
-#'
 #'
 #' @return
 #' \item{\code{pvalue}}{
@@ -27,57 +28,100 @@
 #' \item{\code{trace_after}}{
 #'  Estimated trace after the change
 #' }
-#' @export
 #'
-#' @details This function dates and detects changes in trace of the covariance
-#'  function. This can be interpreted as the changes in the total variation
-#'  of the the functional data. Trace is defined as the infinite sum of
-#'  the eigenvalues of the covariance operator and for the sake of
-#'  implementation purpose, the sum is truncated up to the total number
-#'  of basis functions that defines the functional data at hand. The critical
-#'  values are approximated via \code{M} Monte Carlo simulations.
+#' @noRd
+#' @keywords internal
 #'
-#' @references Aue, A, G Rice, and O Sönmez. “Structural Break Analysis
-#'  for Spectrum and Trace of Covariance Operators.” Environmetrics
-#'  (London, Ont.) 31, no. 1 (2020). \url{https://doi.org/10.1002/env.2617.}
+#' @references Aue, A., Rice, G., & S\"{o}nmez, O. (2020). Structural break
+#'  analysis for spectrum and trace of covariance operators. Environmetrics
+#'  (London, Ont.), 31(1)
 #'
 #' @examples
-#' trace_change(generate_brownian_motion(200,v=seq(0,1,0.05)))
-#' trace_change(electricity)
-trace_change <- function (X, CPs = NULL, M = 1000){
-  X <- .check_data(X)
-  X <- center(X, CPs=CPs)
+#' #.change_trace(generate_brownian_motion(200,v=seq(0,1,0.05)))
+#' #.change_trace(electricity)
+.change_trace <- function(X, CPs = NULL, M = 1000,
+                          statistic = 'Tn', critical='simulation',
+                          blocksize=1, replace=TRUE, type='separate'){
+  X <- center(funts(X), CPs=CPs)
 
   N <- ncol(X$data)
   D <- nrow(X$data)
 
+  tmp <- .trace_statistic(X$data, statistic, location=TRUE)
+  Sn <- tmp[1]
+  k_star <- tmp[2]
+
+  if(critical=='simulation'){
+    # values_sim <- sapply(1:M, function(k,N) abs(sde::BBridge(0, 0, 0, 1, N-1)), N=N )
+    values_sim <- abs(generate_brownian_bridge(M,seq(0,1,length.out=N))$data)
+
+    if(statistic=='Tn'){
+      Values <- dot_integrate_col(values_sim)
+    }else if(statistic=='Mn'){
+      Values <- apply(values_sim, MARGIN = 2, max)
+    }
+  } else if(critical=='permutation'){
+    Values <- .bootstrap(X = X$data, blocksize = blocksize, M = M,
+                         type = type, replace = replace, fn = .trace_statistic,
+                         statistic=statistic)
+  }
+  # Tn <- rep(0,N)
+  # for (k in 1:N) {
+  #   T_x <- sum(Xi[1:k])/N
+  #   Tn[k] <- (1/sigma) * abs(T_x - (k)/N * T_1)
+  # }
+  # Sn <- max(Tn)
+  p <- sum(Sn <= Values) / M # Compute p-value
+  # k_star <- min(which.max(Tn))
+
+  # data1_pca <- pca(funts(X$data[,1:k_star]),TVE=1)
+  # data2_pca <- pca(funts(X$data[,(k_star+1):N]),TVE=1)
+  # tr_before <- sum(data1_pca$sdev)
+  # tr_after <- sum(data2_pca$sdev)
+  list('pvalue' = p,
+       'location' = k_star,
+       'statistic' = Sn,
+       'simulations' = Values
+       )
+}
+
+
+#' Trace Test Statistic
+#'
+#' function for test statistic in trace change point detection
+#'
+#' @inheritParams .change_trace
+#' @param location Boolean if location of change should be returned
+#'
+#' @returns Test statistic and (optional) location of change
+#'
+#' @noRd
+#' @keywords internal
+.trace_statistic <- function(X, statistic, location=FALSE){
+  N <- ncol(X)
+
   Cov_op <- .partial_cov(X, 1)
   lambda <- Cov_op$eigen_val
   T_1 <- sum(lambda)
-  Xi <- sapply(1:N, function(i) X$data[,i] %*% X$data[,i])
+  Xi <- sapply(1:N, function(i) X[,i] %*% X[,i])
   sigma_sq <- sandwich::lrvar(Xi, prewhite = F)
   sigma <- sqrt(sigma_sq)
-  Values <- sapply(1:M, function(k) max(abs(sde::BBridge(0, 0, 0, 1, N-1))))
 
   Tn <- rep(0,N)
   for (k in 1:N) {
     T_x <- sum(Xi[1:k])/N
-    Tn[k] <- (1/sigma) * abs(T_x - (k)/N * T_1)
+    Tn[k] <- (1/sigma) * abs(T_x - k/N * T_1)
   }
-  Sn <- max(Tn)
-  # p <- stats::ecdf(Values)(Sn)
-  p <- sum(Sn <= Values) / M # Compute p-value
-  k_star <- min(which.max(Tn))
 
-  data1_pca <- pca(funts(X$data[,1:k_star]),TVE=1)
-  data2_pca <- pca(funts(X$data[,(k_star+1):N]),TVE=1)
-  tr_before <- sum(data1_pca$sdev)
-  tr_after <- sum(data2_pca$sdev)
-  list(change = k_star/N,
-       location = k_star,
-       pvalue = p,
-       trace_before = tr_before,
-       trace_after = tr_after)
+  if(statistic=='Tn'){
+    Sn <- dot_integrate(Tn)
+  }else if(statistic=='Mn'){
+    Sn <- max(Tn)
+  }
+
+  if(!location) return(Sn)
+
+  c(Sn, min(which.max(Tn)) )
 }
 
 
@@ -88,46 +132,47 @@ trace_change <- function (X, CPs = NULL, M = 1000){
 #'  eigenfunctions of the parial sum estimate, along with the coefficient
 #'  matrix of the estimated covariance function.
 #'
+#' @details This function simply estimates the covariance function based on the partial sum of the centered functional observations.
+#'  The length of the sum is determined by \code{x}, and when \code{x=1} this estimate corresponds to the regular covariance function
+#'  estimates using the whole sample.
 #'
-#' @param fdobj A functional data object of class '\code{fd}'
+#' @inheritParams .change_trace
 #' @param x Fraction of the sample size that the partial sum is computed.
 #'  This input must be in (0,1]. The default is \code{x=1} which corresponds
 #'  to the regular covariance function estimate in functional data analysis.
 #'
-#' @export
+#' @keywords internal
+#' @noRd
 #'
 #' @return
-#'\item{\code{eigen_val}}{
-#' Eigenvalues of the partial sum estimate of the covariance function
-#'}
-#'\item{\code{eigen_fun}}{
-#' Eigenfunctions of the partial sum estimate of the covariance function
-#'}
-#'\item{\code{coef_matrix}}{
-#' Coefficient matrix of the partial sum estimate of the covariance function
-#'}
-#'
-#'@details This function simply estimates the covariance function based on the partial sum of the centered functional observations.
-#'The length of the sum is determined by \code{x}, and when \code{x=1} this estimate corresponds to the regular covariance function
-#'estimates using the whole sample.
+#'  \item{\code{eigen_val}}{
+#'    Eigenvalues of the partial sum estimate of the covariance function
+#'  }
+#'  \item{\code{eigen_fun}}{
+#'    Eigenfunctions of the partial sum estimate of the covariance function
+#'  }
+#'  \item{\code{coef_matrix}}{
+#'    Coefficient matrix of the partial sum estimate of the covariance function
+#'  }
 #'
 #' @seealso \code{\link{pca.fd}}
 #'
 #' @examples
-#' .partial_cov(electricity)
-#' # Estimated eigenvalues
-#' e1 = eigen(autocov_approx_h(electricity,0))
-#' e2 = .partial_cov(funts(electricity),x=1)
-#' sum(round(e1$values,4) != round(e2$eigen_val,4) )
-#' sum(round(e1$vectors,4) != round(e2$eigen_fun,4) )
-#' sum(round(autocov_approx_h(electricity,0),4) != round(e2$coef_matrix,4) )
-#' # e1 and e2 will both estimate the eigenvalues of the covariance
-#' # operator based on the whole sample
+#' #.partial_cov(electricity)
+#' ## Estimated eigenvalues
 #'
-#' # estimates using only 90% of the data
-#' Cov = .partial_cov(electricity, 0.9)
+#' #e1 = eigen(autocovariance(electricity,0))
+#' #e2 = .partial_cov(funts(electricity),x=1)
+#' #sum(round(e1$values,4) != round(e2$eigen_val,4) )
+#' #sum(round(e1$vectors,4) != round(e2$eigen_fun,4) )
+#' #sum(round(autocovariance(electricity,0),4) != round(e2$coef_matrix,4) )
+#' ## e1 and e2 will both estimate the eigenvalues of the covariance
+#' ## operator based on the whole sample
+#'
+#' ## estimates using only 90% of the data
+#' #Cov = .partial_cov(electricity, 0.9)
 .partial_cov <- function(X, x = NULL){
-  X <- .check_data(X)
+  X <- funts(X)
   if (is.null(x)) x <- 1
   if (x>1 | x<=0) stop("x should be in (0,1]")
 
