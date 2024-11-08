e4a77fd3b04d34f5643af3fcce3cd4f4aa4c6f32
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-11-07 21:35:00 -0500

    Update plots

--- R/plot_qq.R
+++ R/plot_qq.R
@@ -1,5 +1,11 @@
-
-#' @rdname qqplot
+#' QQ Plot Generic Function
+#'
+#' @param x Data to examine with qqplot
+#' @param ... Additional paramters based on the data
+#'
+#' @name qqplot
+#'
+#' @seealso [qqplot.funts()] [qqplot.default()]
 #'
 #' @export
 qqplot <- function(x, ...) UseMethod("qqplot")
@@ -9,57 +15,58 @@ qqplot <- function(x, ...) UseMethod("qqplot")
 qqplot.default <- function(x, ...) stats::qqplot(x, ...)
 
 
-
 #' Generic Function to Compute QQ plot for funts
 #'
 #' Creates normal QQ plots on the principal components of functional data
 #'
 #' @param x funts object
-#' @param TVE Numeric in [0,1] giving the total variance explained for selecting
+#' @param TVE Numeric in \[0,1\] giving the total variance explained for selecting
 #'  the number of principal components.
-#' @param max.d Max number of principal components
-#' @param qq.sep Boolean for if the QQ plots should be seperate or stacked
-#' @param alpha Significant
-#' @param ...
+#' @param d.max Max number of principal components
+#' @param alpha Significance level, alpha in \[0,1\]
+#' @param CPs Vector of change points
+#' @param legend Boolean indicating if legend should be shown
+#' @param ... Unused
 #'
 #' @return ggplot2 for QQ plot
 #' @export
 #'
 #' @examples
 #' result <- qqplot(funts(electricity))
-qqplot.funts <- function(x, TVE=0.95, max.d=3, qq.sep=TRUE, alpha = 0.05, ...){
-  distplot(X = x, TVE = TVE, max.d = max.d, distribution = 'norm',
-           qq.sep = qq.sep, alpha = alpha, ...)
+qqplot.funts <- function(x, TVE=0.95, d.max=3, alpha = 0.05,
+                         CPs=NULL, legend = FALSE, ...){
+  .plot_distribution(X = x, TVE = TVE, d.max = d.max, distribution = 'norm',
+                     alpha = alpha, CPs=CPs, legend=legend, ...)
 }
+
 
 #' Distribution plot
 #'
-#' @param x description
-#' @param CPs description
-#' @param alpha description
-#' @param TVE description
-#' @param max.d description
-#' @param distribution description
-#' @param qq.sep description
-#' @param alpha description
-#' @param legend description
-#' @param ... description
+#' @param X funts object
+#' @param CPs Vector of change points
+#' @param TVE Total variance explained. Value in \[0,1\]
+#' @param d.max Numberic for max number of pca components to examine
+#' @param distribution String for distribution to consider. Any distribution with
+#'  d\code{distribution} and q\code{distribution} defined can be used
+#' @param alpha Significance level, alpha in \[0,1\]
+#' @param legend Boolean indicating if legend should be shown
+#' @param ... Additional parameters for the distribution
 #'
 #' @return ggplot2 for plot to compare principal components to a distribution
-#' @export
 #'
 #' @references John Fox, & Sanford Weisberg (2019). An R Companion to Applied
 #'  Regression. Sage.
 #'
 #' @examples
-#' result <- distplot(electricity)
-#' result1 <- distplot(electricity, max.d=2, qq.sep=FALSE)
-#' result2 <- distplot(electricity,dist='exp')
-distplot <- function(X, CPs=NULL, TVE=0.95, max.d = 3,
-                           distribution = "norm",
-                           qq.sep = TRUE,
-                           alpha = 0.05, legend = FALSE, ...){
-  X <- .check_data(X)
+#' # result <-  .plot_distribution(electricity)
+#' # result1 <- .plot_distribution(electricity,distribution='exp', CPs=c(50,175))
+#'
+#' @noRd
+#' @keywords internal
+.plot_distribution <- function(X, CPs=NULL, TVE=0.95, d.max = 3,
+                               distribution = "norm",
+                               alpha = 0.05, legend = FALSE, ...){
+  X <- funts(X)
 
   # Parameters
   #   All data will be of length n
@@ -69,17 +76,18 @@ distplot <- function(X, CPs=NULL, TVE=0.95, max.d = 3,
   # Convert X to pca
   if(is.null(CPs)){
     X_pca <- pca(X,TVE = TVE)
-    D <- min(ncol(X_pca$x), max.d)
+    D <- min(ncol(X_pca$x), d.max)
 
     dat <- X_pca$x[,1:D]
   }else{
     CPs <- unique(c(0, CPs, ncol(X)))
     max_n <- max(CPs[-1] - CPs[-length(CPs)])#max((CPs-lag(CPs))[-1])
-    D <- length(CPs)-1
-    dat <- data.frame(matrix(nrow=max_n,ncol=D))
+    D <- min(nrow(X), d.max)
+    dat <- list()
     for(d in 1:(length(CPs)-1)){
-      X_pca <- pca(funts(X$data[,(CPs[d]+1):CPs[d+1]]), TVE = 0)
-      dat[,d] <- c(X_pca$x,rep(NA,length.out=max_n-length(X_pca$x)))
+      X_pca <- pca(funts(X$data[,(CPs[d]+1):CPs[d+1]]), TVE = TVE)
+      dat[[d]] <- X_pca$x[,1:D]
+      # dat[,d] <- c(X_pca$x,rep(NA,length.out=max_n-length(X_pca$x)))
     }
 
   }
@@ -95,34 +103,61 @@ distplot <- function(X, CPs=NULL, TVE=0.95, max.d = 3,
   # Each components
   results <- results_coefs <- data.frame()
   for(d in 1:D){
-    data_comp <- stats::na.omit(dat[,d])
-    n <- length(data_comp)
-    P <- stats::ppoints(n)
+    n <- c()
+    P <- list()
 
-    ord <- order(data_comp)
-    df <- data.frame('ord' = data_comp[ord],
-                     'z' = q.function(P))#, ...))
+    if(!is.null(CPs)){
+      df <- data.frame()
+      for(i in 1:(length(CPs)-1)){
+        data_comp <- stats::na.omit(dat[[i]][,d])
+        n <- c(n,length(data_comp))
+        P[[i]] <- stats::ppoints(n[i])
+
+        ord <- order(data_comp)
+        df <- rbind(df,
+                    data.frame(
+                      'CP' = paste0(CPs[i]+1,'-',CPs[i+1]),
+                      'ord' = data_comp[ord],
+                      'z' = q.function(P[[i]], ...)))
+      }
+    }else{
+      data_comp <- stats::na.omit(dat[,d])
+      n <- length(data_comp)
+      P[[1]] <- stats::ppoints(n)
+
+      ord <- order(data_comp)
+      df <- data.frame('CP'=paste0('1-',ncol(X)),
+                       'ord' = data_comp[ord],
+                       'z' = q.function(P[[1]], ...))
+    }
 
     # Line
     #   c(0.25, 0.75) for x-axis on plot
-    Q.x <- stats::quantile(df$ord, c(0.25, 0.75))
-    Q.z <- q.function(c(0.25, 0.75))#, ...)
-    b <- diff(Q.x)/diff(Q.z)
-    coef <- c(Q.x[1] - b * Q.z[1], b)
+    groups <- unique(df$CP)
+    for(i in 1:length(groups)){
+      Q.x <- stats::quantile(df[df$CP==groups[i],'ord'], c(0.25, 0.75))
+      Q.z <- q.function(c(0.25, 0.75), ...)
+      b <- diff(Q.x)/diff(Q.z)
+      coef <- c(Q.x[1] - b * Q.z[1], b)
 
-    # Interval
-    SE <- (coef[2]/d.function(df$z)) * sqrt(P * (1 - P)/n)
-    fit.value <- coef[1] + coef[2] * df$z
-    df$upper <- fit.value + Z_alpha * SE
-    df$lower <- fit.value - Z_alpha * SE
+      # Interval
+      SE <- (coef[2]/d.function(df[df$CP==groups[i],'z'], ...)) * sqrt(P[[i]] * (1 - P[[i]])/n[i])
+      fit.value <- coef[1] + coef[2] * df[df$CP==groups[i],'z']
+      df[df$CP==groups[i],'upper'] <- fit.value + Z_alpha * SE
+      df[df$CP==groups[i],'lower'] <- fit.value - Z_alpha * SE
+
+      # Setup For Plot
+      results_coefs <- rbind(results_coefs,
+                             data.frame('CP'=groups[i],'d'=paste0('Dim-',d),
+                                        'i'=coef[1],'s'=coef[2]))
+    }
 
     # Label Points outside interval
     # df$label <- ifelse(df$ord > df$upper | df$ord < df$lower, X$labels[ord], "")
     results <- rbind(results, data.frame('d'=paste0('Dim-',d), df))
-    results_coefs <- rbind(results_coefs,data.frame('d'=paste0('Dim-',d),'i'=coef[1],'s'=coef[2]))
   }
 
-  if(qq.sep){
+  if(!is.null(CPs)){
     p <- ggplot2::ggplot() +
       ggplot2::facet_grid(row=ggplot2::vars(d),scales='free_y')
   }else{
@@ -132,23 +167,58 @@ distplot <- function(X, CPs=NULL, TVE=0.95, max.d = 3,
     p <- p +
       ggplot2::guides(fill='none',
                       color='none')
+  }else{
+    if(!is.null(CPs)){
+      # Order legend
+      p <- p +
+        ggplot2::scale_fill_discrete(breaks=unique(results$CP),
+                                     guide=ggplot2::guide_legend(override.aes = list(linetype = 0))) +
+        ggplot2::scale_color_discrete(breaks=unique(results$CP))
+    }else{
+      # Order legend
+      p <- p +
+        ggplot2::scale_fill_discrete(breaks=unique(results$d),
+                                     guide=ggplot2::guide_legend(override.aes = list(linetype = 0))) +
+        ggplot2::scale_color_discrete(breaks=unique(results$d))
+    }
   }
 
-  p +
-    ggplot2::geom_ribbon(ggplot2::aes(x=z, ymin = lower, ymax = upper, color=d, fill=d),
-                data=results, alpha=0.2) +
-    ggplot2::geom_abline(ggplot2::aes(intercept = i, slope = s,
-                    color=d), data= results_coefs) +
-    ggplot2::geom_point(ggplot2::aes(x=z,y=ord,color=d,fill=d),data=results) +
-    ggplot2::theme_bw() +
-    ggplot2::theme(axis.text = ggplot2::element_text(size=18),
-                   axis.text.y = ggplot2::element_blank()) +
-    ggplot2::labs(fill='',color='') +
-    ggplot2::xlab(NULL) +
-    ggplot2::ylab(NULL) +
-    ggplot2::scale_x_continuous(breaks=floor(min(results$z)):ceiling(max(results$z)))
+  # Remove warnings
+  z <- lower <- upper <- i <- s <- CP <- NULL
+
+  if(!is.null(CPs)){
+    p <- p +
+      ggplot2::geom_ribbon(ggplot2::aes(x=z, ymin = lower, ymax = upper, color=CP, fill=CP),
+                           data=results, alpha=0.2) +
+      ggplot2::geom_abline(ggplot2::aes(intercept = i, slope = s,
+                                        color=CP), data= results_coefs) +
+      ggplot2::geom_point(ggplot2::aes(x=z,y=ord,color=CP,fill=CP),data=results) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(axis.text = ggplot2::element_text(size=18),
+                     axis.text.y = ggplot2::element_blank()) +
+      ggplot2::labs(fill='',color='') +
+      ggplot2::xlab(NULL) +
+      ggplot2::ylab(NULL) +
+      ggplot2::scale_x_continuous(breaks=floor(min(results$z)):ceiling(max(results$z)))
+  } else{
+    p <- p +
+      ggplot2::geom_ribbon(ggplot2::aes(x=z, ymin = lower, ymax = upper, color=d, fill=d),
+                           data=results, alpha=0.2) +
+      ggplot2::geom_abline(ggplot2::aes(intercept = i, slope = s,
+                                        color=d), data= results_coefs) +
+      ggplot2::geom_point(ggplot2::aes(x=z,y=ord,color=d,fill=d),data=results) +
+      ggplot2::theme_bw() +
+      ggplot2::theme(axis.text = ggplot2::element_text(size=18),
+                     axis.text.y = ggplot2::element_blank()) +
+      ggplot2::labs(fill='',color='') +
+      ggplot2::xlab(NULL) +
+      ggplot2::ylab(NULL) +
+      ggplot2::scale_x_continuous(breaks=floor(min(results$z)):ceiling(max(results$z)))
+  }
+
   # if(!is.null(labels)) p <- p + geom_text( aes(label = label))
   # print(p)
+  p
 }
 
 
