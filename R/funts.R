5b39208ac6f2f992664da26975bd88dd9f8bbf2a
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-11-16 21:37:22 -0500

    update data

--- R/funts.R
+++ R/funts.R
@@ -1,137 +1,242 @@
-#' Define funts Object
+#' Funts Objects
 #'
-#' @param x Data.frame to convert into data
-#' @param labels Labels for the observations. Defaults to the column names of
-#'  \code{x}.
+#' Define data as, check data if, and convert data to funts object. The data
+#'  may be from another popular implementation.
+#'
+#' @param X Data to convert into funts object. Options include: data.frame,
+#'  matrix, array, fda::fd, fda.usc::fdata, rainbow::fts (used in ftsa),
+#'  rainbow::fds (used in ftsa), funData::funData, and fChange::funts. For a
+#'  matrix, each column is a unique observation, at the rows are the observed
+#'  intra-observation (i.e. resolution) points.
+#' @param name String for the name of the object. Defaults to the name of the
+#'  input variable.
+#' @param labels Labels for the observations. Defaults to the column names or
+#'  names inside of the object X.
 #' @param intraobs Vector of numerics indicating the points for each
-#'  observation. Defaults to even spacing on [0,1], but may be uneven or
-#'  for any real numbers.
+#'  observation. Defaults to even spacing on \[0,1\], or those included in the
+#'  object. These may be unevenly spaced.
+#' @param inc.warnings Boolean on if warnings should be given. Defaults to TRUE,
 #'
-#' @return funts object
+#' @name funts
+#'
+#' @return funts / as.funts: funts object
 #' @export
 #'
 #' @examples
-#' funts(electricity)
-#' funts(generate_brownian_motion(100, c(0,0.1,0.25,0.5,1)))
+#' bm <- funts(generate_brownian_motion(100, c(0,0.1,0.25,0.5,1)))
+#'
+#' # Electricity
+#' result <- funts(electricity)
 funts <- function(X, name=NULL,
-                  labels=colnames(as.data.frame(X)),
-                  intraobs=seq(0,1,length.out=nrow(X))){
-  if(is.funts(X)) return(X)
-  # TODO:: Get name of variable
-  # deparse(substitute(x))
+                  labels=NULL,
+                  intraobs=NULL,
+                  inc.warnings=TRUE){
+  ## Return if already right object
+  if(is.funts(X)) {
+    if(inc.warnings & sum(is.na(X$data))>0)
+      warning('NA values in data may affect some methods',call. = FALSE)
 
+    return(.validate.funts(X))
+  }
 
-  # Note: as.matrix is very important! Otherwise computation is
-  #   far more expensive!
-  funts_obj <- list(
-    'data' = as.matrix(X),
-    'name' = name,
-    'labels' = labels,
-    'intraobs' = intraobs
-  )
+  ## Fill-in Data
+  if(is.null(name))
+    name <- deparse(substitute(X))
+
+  # Setup Data in Different Scenarios
+  # Note: as.matrix is very important in data!
+  #   Otherwise computation is far more expensive!
+  if(requireNamespace("fda", quietly = TRUE) &&
+     fda::is.fd(X)){
+    if(is.null(labels))
+      labels <- X$fdnames$reps
+    if(is.null(intraobs)){
+      ran_vals <- X$basis$rangeval
+      intraobs <- seq(ran_vals[1],ran_vals[2],
+                      length.out=length(X$fdnames$time))
+    }
+
+    funts_obj <- list(
+      'data' = as.matrix(fda::eval.fd(intraobs, fdobj = X)),
+      'name' = name,
+      'labels' = labels,
+      'intraobs' = intraobs
+    )
+  } else if(requireNamespace("fda.usc", quietly = TRUE) &&
+            fda.usc::is.fdata(X)){
+    if(is.null(labels))
+      labels <- rownames(X$data)
+    if(is.null(intraobs))
+      intraobs <- X$argvals
+
+    funts_obj <- list(
+      'data' = as.matrix(t(X$data)),
+      'name' = name,
+      'labels' = labels,
+      'intraobs' = intraobs
+    )
+  } else if(requireNamespace('rainbow', quietly = TRUE) &&
+            (inherits(X, "fts") || inherits(X, "fds"))){
+    if(is.null(labels)){
+      if(inherits(X, "fts"))
+        labels <- X$time
+      if(inherits(X, "fds"))
+        labels <- colnames(X$y)
+    }
+    if(is.null(intraobs))
+      intraobs <- X$x
+
+    funts_obj <- list(
+      'data' = as.matrix(X$y),
+      'name' = name,
+      'labels' = labels,
+      'intraobs' = intraobs
+    )
+  } else if(requireNamespace('funData', quietly = TRUE) &&
+            inherits(X, "funData")){
+    if(is.null(labels))
+      labels <- rownames(X@X)
+    if(is.null(intraobs))
+      intraobs <- X@argvals[[1]]
+
+    funts_obj <- list(
+      'data' = as.matrix(t(X@X)),
+      'name' = name,
+      'labels' = labels,
+      'intraobs' = intraobs
+    )
+  } else if(inherits(X, "matrix") ||
+            inherits(X, "data.frame") ||
+            inherits(X, "array")){
+    if(is.null(intraobs))
+      intraobs <- seq(0,1,length.out=nrow(X))
+    if(is.null(labels))
+      labels <- colnames(as.data.frame(X))
+
+    funts_obj <- list(
+      'data' = as.matrix(X),
+      'name' = name,
+      'labels' = labels,
+      'intraobs' = intraobs
+    )
+  } else if(inherits(X, "numeric")){
+    if(is.null(intraobs))
+      intraobs <- seq(0,1,length.out=1)
+    if(is.null(labels)){
+      if(is.null(names(X))){
+        labels <- 1:length(X)
+      }else{
+        labels <- names(X)
+      }
+    }
+    funts_obj <- list(
+      'data' = matrix(X,nrow=1),
+      'name' = name,
+      'labels' = labels,
+      'intraobs' = intraobs
+    )
+  } else{
+    stop('Data type not supported.',call. = FALSE)
+  }
+
+  ## Clean-up
+  #   Old: structure(x, 'labels' = labels, class = "funts")
   colnames(funts_obj$data) <- NULL
-  # structure(x, 'labels' = labels, class = "funts")
+  rownames(funts_obj$data) <- NULL
   class(funts_obj) <- 'funts'
 
-  funts_obj
+  ## Verification
+  # Note if NAs are present
+  if(length(funts_obj$intraobs)!=nrow(funts_obj$data))
+    stop('Resolution does not match data (rows)',call. = FALSE)
+  if(length(funts_obj$labels)!=ncol(funts_obj$data))
+    warning('Labels do not match data (cols)',call. = FALSE)
+
+  if(inc.warnings & sum(is.na(funts_obj$data))>0)
+    warning('NA values in data may affect some methods',call. = FALSE)
+
+  .validate.funts(funts_obj)
 }
 
 
-#' Check data to see if its funts
-#'
-#' @param x Data to examine class
-#'
-#' @return Boolean indicating if \code{x} is a funts object or not
+#' @rdname funts
 #' @export
 #'
 #' @examples
-#' # is.funts(electricity)
-#' # is.funts(funts(electricity))
-is.funts <- function(x){
-  inherits(x, "funts") #& length(x$x) > 0
+#' result <- as.funts(electricity)
+as.funts <- function(X){
+  funts(X)
 }
 
 
-#' Convert to funts
-#'
-#' @param x Object to convert to funts
-#'
-#' @return funts object
+#' @rdname funts
 #' @export
 #'
+#' @return is.funts: Boolean indicating if \code{x} is a funts object or not
+#'
 #' @examples
-#' # as.funts(electricity)
-as.funts <- function(x){
-  switch(class(x)[[1]],
-         'data.frame'={
-           funts(x)
-         },
-         'matrix'={
-           funts(x)
-         },
-         {
-           stop('Only matrix and data.frame objects currently setup')
-         }
-  )
+#' result <- is.funts(electricity)
+is.funts <- function(X){
+  inherits(X, "funts")
 }
+
 
 
 #' Validate funts
 #'
-#' @param x funts object to verify for any violations
+#' Internal function to check lengths and data information is right
+#'
+#' @param X funts object to verify for any violations
 #'
 #' @return Silently returns the data if there are no violations
-#' @export
 #'
-#' @examples
-#' # validate.funts(funts(electricity))
-validate.funts <- function(x){
-  if (!inherits(x, "funts")) stop("Objects must be of class funts")
-  values <- unclass(x)$data
-  labels <- x$labels
+#' @keywords internal
+#' @noRd
+.validate.funts <- function(X){
+  # Class
+  if (!inherits(X, "funts")) stop("Objects must be of class funts")
 
-  if (length(labels) != ncol(values)) {
-    stop("There must be the same number of `labels` as values in `x`")
-  }
+  # Sizes
+  if(ncol(X) != length(X$labels))
+    stop("Data must have the same number of columns as the length of labels")
+  if(nrow(X) != length(X$intraobs))
+    stop("Data must have the same number of rows as the length of intraobs")
 
-  invisible(x)
+  X
 }
 
 
-#' Math Group for funts
+
+#' Group Generic Functions
 #'
-#' @param x funts object
-#' @param ... Additional parameters for functions in math group
+#' Group generic methods defined for things like Math, Ops, and so forth.
 #'
-#' @return funts object with applied functions
+#' @param x,e1,e2 funts object
+#' @param ... Further arguments passed to the methods
+#'
+#' @return funts object
 #' @export
 #'
+#' @name funts_group
+#'
 #' @examples
-#' # sqrt( funts(electricity) )
+#' result <- sqrt( funts(electricity) )
 Math.funts <- function (x, ...) {
-  validate.funts(x)
-
   x$data <- methods::callGeneric(x$data,...)
 
   x
 }
 
 
-#' Ops Group for funts
-#'
-#' TODO: Figure out for non-intersections.
-#'
-#' @param e1 funts object
-#' @param e2 funts object
-#'
-#' @return funts object from the two elements
+#' @rdname funts_group
 #' @export
 #'
 #' @examples
-#' # funts(electricity) + funts(electricity)
-#' # funts(electricity) * funts(electricity)
+#' result <- funts(electricity) + funts(electricity)
+#' result <- funts(electricity) * funts(electricity)
 Ops.funts <- function (e1, e2) {
   # Combines the columns that are the same
+  # TODO: Figure out for non-intersections.
 
   if(missing(e2)) {
     .Class <- "matrix"
@@ -200,6 +305,8 @@ Ops.funts <- function (e1, e2) {
 
 #' Difference funts
 #'
+#' Difference the functional data at some lag / to some degree.
+#'
 #' @param x funts object
 #' @param lag An integer indicating which lag to use.
 #' @param differences	An integer indicating the order of the difference.
@@ -209,8 +316,8 @@ Ops.funts <- function (e1, e2) {
 #' @export
 #'
 #' @examples
-#' # diff(funts(electricity), lag=1)
-#' # diff(funts(electricity), differences=2)
+#' result <- diff(funts(electricity), lag=1)
+#' result <- diff(funts(electricity), differences=2)
 diff.funts <- function(x, lag = 1L, differences = 1L, ...) {
   if(differences==0) return(x)
 
@@ -225,7 +332,6 @@ diff.funts <- function(x, lag = 1L, differences = 1L, ...) {
   dat1 <- data.frame(matrix(NA,
                             nrow = nrow(x$data),
                             ncol = ncol(x$data)-lag))
-  #colnames(dat1) <- x$labels[-c(length(x$labels)-(lag-1):0)]
   for(i in 1:(ncol(x$data)-lag)){
     dat1[,i] <- x$data[,i+lag]-x$data[,i]
   }
@@ -234,85 +340,165 @@ diff.funts <- function(x, lag = 1L, differences = 1L, ...) {
 }
 
 
-#' Max funts
+#' Max / Min for funts Objects
 #'
-#' Get the observation(s) with the max average value.
-#'
-#' TODO:: Add a pw option
+#' Get the observation(s) with the min / max values. Selected as the observation
+#'  with the largest / smallest mean or the pointwise values.
 #'
 #' @param x funts object
+#' @param type String indicating if finding for observation ('Obs', default),
+#'  or for pointwise values ('Intraobs').
+#' @param na.rm Boolean if NA values should be removed. Defaults to TRUE.
+#' @param ... Additional parameters to stats function
+#'
+#' @return A funts object
+#' @export
+#'
+#' @name minmax
+#'
+#' @examples
+#' results <- max(funts(electricity))
+max.funts <- function(x, type=c('Obs','Intraobs'), na.rm=TRUE, ...){
+  poss_types <- c('Obs','Intraobs')
+  type <- .verify_input(type, poss_types)
+
+  if(type == 'Obs'){
+    idx <- which.max(colMeans(x$data,na.rm = na.rm))
+    return_dat <- funts(x$data[,idx,drop=FALSE],
+                        name = x$name,
+                        intraobs = x$intraobs,
+                        labels = x$labels[idx])
+  }else if(type=='Intraobs'){
+    dat <- apply(x$data,MARGIN = 1, max, na.rm=na.rm, ...)
+    return_dat <- funts(matrix(dat),
+                        name = x$name,
+                        intraobs = x$intraobs)
+
+  }
+
+  return_dat
+}
+
+
+#' @rdname minmax
+#' @export
+#'
+#' @examples
+#' results <- min(funts(electricity))
+min.funts <- function(x, type=c('Obs','Intraobs'), na.rm=TRUE, ...){
+  poss_types <- c('Obs','Intraobs')
+  type <- .verify_input(type, poss_types)
+
+  if(type == 'Obs'){
+    idx <- which.min(colMeans(x$data,na.rm=na.rm))
+    return_dat <- funts(x$data[,idx,drop=FALSE],
+                        name = x$name,
+                        intraobs = x$intraobs,
+                        labels = x$labels[idx])
+  }else if(type=='Intraobs'){
+    dat <- apply(x$data,MARGIN = 1, min, na.rm=na.rm, ...)
+    return_dat <- funts(matrix(dat),
+                        name = x$name,
+                        intraobs = x$intraobs)
+
+  }
+
+  return_dat
+}
+
+
+#' Average Functions for funts Objects
+#'
+#' Compute the average values for funts objects
+#'
+#' @inheritParams minmax
+#'
+#' @return Numeric vector
+#' @export
+#'
+#' @name average
+#'
+#' @examples
+#' results <- mean(funts(electricity))
+mean.funts <- function(x, na.rm=TRUE, ...) {
+  rowMeans(x$data,na.rm = na.rm, ...)
+}
+
+
+#' @rdname average
+#' @export
+#'
+#' @importFrom stats median
+#'
+#' @examples
+#' results <- median(funts(electricity))
+median.funts <- function(x, na.rm=TRUE, ...) {
+  apply(x$data,MARGIN = 1, stats::median, na.rm=na.rm, ...)
+}
+
+
+#' Quantile funts
+#'
+#' Obtain the pointwise quantile information of funts objects
+#'
+#' @param x A funts object
+#' @param probs Numerics in \[0,1\] indicating the probabilities of interest
+#' @param ... Additional parameters to pass into quantile function
+#'
+#' @return Matrix with columns for each requested quantile
+#' @export
+#'
+#' @importFrom stats quantile
+#'
+#' @examples
+#' result <- quantile(funts(electricity))
+#' result <- quantile(funts(electricity),probs = 0.95)
+quantile.funts <- function(x, probs = seq(0, 1, 0.25), ...){
+  if(length(probs)>1){
+    output <- t(apply(x$data, MARGIN = 1, stats::quantile, probs=probs, ...))
+  } else{
+    output <- data.frame(apply(x$data, MARGIN = 1, stats::quantile, probs=probs, ...))
+    colnames(output) <- paste0(probs*100,'%')
+  }
+
+  output
+}
+
+
+#' Lag funts
+#'
+#' @param x funts object
+#' @param k integer indicating the number of lags for the data
 #' @param ... Unused additional parameters
 #'
 #' @return A funts object
 #' @export
 #'
+#' @importFrom stats lag
+#'
 #' @examples
-#' # max(funts(electricity))
-max.funts <- function(x, ...){
-  idx <- which.max(colMeans(x$data))
-  funts(x$data[,idx,drop=FALSE],
-        labels = x$labels[idx])
+#' result <- lag(funts(electricity))
+lag.funts <- function(x, k=1, ...) {
+  stopifnot(k >= 0)
+  #ans <- .Call("lag", x, as.integer(k),PACKAGE="fts")
+
+  dat1 <- x$data[,-c(1:round(k))]
+  funts(dat1, labels = x$labels[-c(1:round(k))])
 }
 
 
-#' Min funts
+#' Dimension of funts Object
 #'
-#' Get the observation(s) with the min average value
+#' Retrieve the dimension of a funts object.
 #'
-#' TODO:: Add a pw option
-#'
-#' @inheritParams max.funts
-#'
-#' @return A funts object
-#' @export
-#'
-#' @examples
-#' # min(funts(electricity))
-min.funts <- function(x, ...){
-  idx <- which.min(colMeans(abs(x$data)))
-  funts(x$data[,idx,drop=FALSE],
-        labels = x$labels[idx])
-}
-
-
-#' Mean for funts
-#'
-#' @inheritParams max.funts
-#'
-#' @return Numeric for the mean at each resolution
-#' @export
-#'
-#' @examples
-#' # mean(funts(electricity))
-mean.funts <- function(x, ...) {
-  rowMeans(x$data)
-}
-
-
-#' Dimension of funts
-#'
-#' @inheritParams max.funts
+#' @inheritParams minmax
 #'
 #' @return Numerics indicating the dimension of the funts object
 #' @export
 #'
 #' @examples
-#' # dim(funts(electricity))
+#' results <- dim(funts(electricity))
 dim.funts <- function(x, ...) {
   dim(x$data)
 }
 
-
-#' Plot funts
-#'
-#' @param x A funts object
-#' @param ... Additional parameters to pass into plotting
-#'
-#' @return Plot
-#' @export
-#'
-#' @examples
-#' # plot(funts(electricity))
-plot.funts <- function(x, ...){
-  plot_fd(x$data, ...)
-}
