8b50597e629c3c4a6ea2f3ede93cac4e3e9fe19d
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-11-21 21:38:28 -0500

    update general

--- R/data_verification.R
+++ R/data_verification.R
@@ -1,30 +1,30 @@
-#' Check data for functions
-#'
-#' @param X funts or object that can be directly converted into funts object
-#'
-#' @return funts object or error if the data is incorrect
-#'
-#' @examples
-#' #.check_data(funts(electricity))
-#' #.check_data(electricity)
-#'
-#' @keywords internal
-#' @noRd
-.check_data <- function(X, check.na=TRUE){
-  if(class(X)[1]=='funts') {
-    if(check.na & sum(is.na(X$data))>0)
-      warning('NA values in data may affect some methods',call. = FALSE)
-    return(X)
-  }
-
-  if('data.frame' %in% class(X) || 'matrix' %in% class(X)){
-    if(check.na & sum(is.na(X))>0)
-      warning('NA values in data may affect some methods',call. = FALSE)
-    return(funts(X))
-  }
-
-  stop('Check format of input data',call. = FALSE)
-}
+# #' Check data for functions
+# #'
+# #' @param X funts or object that can be directly converted into funts object
+# #'
+# #' @return funts object or error if the data is incorrect
+# #'
+# #' @examples
+# #' #.check_data(funts(electricity))
+# #' #.check_data(electricity)
+# #'
+# #' @keywords internal
+# #' @noRd
+# .check_data <- function(X, check.na=TRUE){
+#   if(class(X)[1]=='funts') {
+#     if(check.na & sum(is.na(X$data))>0)
+#       warning('NA values in data may affect some methods',call. = FALSE)
+#     return(X)
+#   }
+#
+#   if('data.frame' %in% class(X) || 'matrix' %in% class(X)){
+#     if(check.na & sum(is.na(X))>0)
+#       warning('NA values in data may affect some methods',call. = FALSE)
+#     return(funts(X))
+#   }
+#
+#   stop('Check format of input data',call. = FALSE)
+# }
 
 
 #' Verify Inputs
@@ -40,12 +40,18 @@
 #' @noRd
 .verify_input <- function(selection, poss_selections){
   # TODO:: Use this in functions instead of repeating code
-  final_selection <- poss_selections[min(pmatch(selection, poss_selections))]
+
+  selection1 <- tolower(selection)
+  poss_selections1 <- tolower(poss_selections)
+  selection1 <- selection1[selection1 %in% poss_selections1]
+
+  final_selection <- poss_selections[pmatch(selection1, poss_selections1)[1]]
 
   if(is.na(final_selection)){
     stop("Issue on the input '",deparse(substitute(selection)),
          "'. See documentation and verify your choice.",
          call. = FALSE)
   }
+
   final_selection
 }
