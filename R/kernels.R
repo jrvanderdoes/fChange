8746932391dd431acb2b717151b605e21b02ce43
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-11-28 21:39:47 -0500

    update general

--- R/kernels.R
+++ R/kernels.R
@@ -11,7 +11,7 @@
 #' @return A numeric value indicating value of Kernel at that point
 #' @export
 #'
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#' @references Horv\'{a}tth, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -35,7 +35,7 @@ truncated_kernel <- function(x) {
 #' @return A numeric value indicating value of Kernel at that point
 #' @export
 #'
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#' @references Horv\'{a}th, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -59,7 +59,7 @@ bartlett_kernel <- function(x) {
 #' @return A numeric value indicating value of Kernel at that point
 #' @export
 #'
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#' @references Horv\'{a}th, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -88,7 +88,7 @@ parzen_kernel <- function(x) {
 #' @return A numeric value indicating value of Kernel at that point
 #' @export
 #'
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#' @references Horv\'{a}th, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -115,7 +115,7 @@ tukey_hanning_kernel <- function(x) {
 #' @return A numeric value indicating value of Kernel at that point
 #' @export
 #'
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#' @references Horv\'{a}th, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -139,7 +139,7 @@ quadratic_spectral_kernel <- function(x) {
 #' @return A numeric value indicating value of Kernel at that point
 #' @export
 #'
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#' @references Horv\'{a}th, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -170,11 +170,11 @@ daniell_kernel <- function(x) {
 #' @references L. Horvath, P. Kokoszka, G. Rice (2014) "Testing stationarity of functional
 #'  time series", Journal of Econometrics, 179(1), 66-82.
 #' @references Politis, D. N. (2003). Adaptive bandwidth choice. Journal of Nonparametric
-#'  Statistics, 15(4–5), 517–533. \url{https://doi.org/10.1080/10485250310001604659}
+#'  Statistics, 15(4-5), 517-533. \url{https://doi.org/10.1080/10485250310001604659}
 #' @references Politis, D. N. (2011). HIGHER-ORDER ACCURATE, POSITIVE SEMIDEFINITE
 #'  ESTIMATION OF LARGE-SAMPLE COVARIANCE AND SPECTRAL DENSITY MATRICES.
-#'  Econometric Theory, 27(4), 703–744. \url{http://www.jstor.org/stable/27975501}
-#' @references Horváth, L., & Rice, G. (2024). Change Point Analysis for Time
+#'  Econometric Theory, 27(4), 703-744. \url{http://www.jstor.org/stable/27975501}
+#' @references Horv\'{a}th, L., & Rice, G. (2024). Change Point Analysis for Time
 #'  Series (1st ed. 2024.). Springer Nature Switzerland.
 #'  \url{https://doi.org/10.1007/978-3-031-51609-2}
 #'
@@ -184,3 +184,70 @@ flat_top_kernel <- function(x) {
   # TODO:: Setup functions to allow FT with arbitrary width
   ifelse(is.nan(x), 1, pmin(1, pmax(1.1 - abs(x), 0)) )
 }
+
+
+#' Adaptive_bandwidth
+#'
+#' Computes the "optimal" bandwidth using a bandwidth selection method based on the
+#'   spectral density operator which adapts to the functional data.
+#'
+#' @param X Functional data object
+#' @param kernel Kernel function selection to use, 'bartlett' or 'parzen'.
+#'
+#' @return a scalar value of the "optimal" data-adapted bandwidth.
+#' @export
+adaptive_bandwidth <- function(X, kernel='bartlett') {
+  X <- funts(X)
+  f_data <- X$data
+
+  ## TODO:: Setup to not be hidden
+  J <- NROW(f_data)
+  N <- NCOL(f_data)
+
+  if (tolower(kernel) == 'bartlett') {
+    kernel_fun <- bartlett_kernel
+    order <- 1
+    xi <- 1
+    kern_int <- 2 / 3
+  } else if (tolower(kernel) == 'parzen') {
+    kernel_fun <- parzen_kernel
+    order <- 2
+    xi <- 6
+    kern_int <- 151 / 280
+    # } else if (kernel == 'Daniell') {
+    # kernel_fun <- daniell_kernel
+    # order <- 2
+    # xi <- (pi^2) / 6
+    # kern_int <- 1
+  } else {
+    stop('Please see the documentation for supported kernels.')
+  }
+
+  data_inner_prod <- crossprod(f_data) / (N * J)
+  C_hat_HS <- numeric(0)
+  for (j in 0:(N-1)) {
+    C_hat_HS[j+1] <- sum( data_inner_prod[(j+1):N,(j+1):N] *
+                          data_inner_prod[1:(N-j),1:(N-j)] )
+  }
+
+  initial_band_q <- 4 * N^(1 / (2*order +1))
+  k_n_j_q <- kernel_fun(1:(N-1) / initial_band_q)
+  initial_band_0 <- initial_band_q / 4
+  k_n_j_0 <- kernel_fun(1:(N-1) / initial_band_0)
+
+  Q_hat_sq <- 2 * sum(k_n_j_0^2 * C_hat_HS[-1])
+  Q_hat_sq <- Q_hat_sq + sum(C_hat_HS[0])
+
+  Term1 <- 2 * sum(k_n_j_q^2 * ((1:(N-1))^(2*order)) * C_hat_HS[-1])
+  Term2 <- Q_hat_sq
+  C_hat_TR <- numeric(0)
+  for (j in 0:(N-1)) {
+    C_hat_TR[j+1] <- sum(data_inner_prod[1:(N-j), (j+1):N])
+  }
+  trace <- 2 * sum(k_n_j_0^2 * C_hat_TR[-1])
+  Term3 <- trace + sum(C_hat_TR[1])
+  band_constant <- (2 * order * xi^2 * Term1 / (kern_int * (Term2 + Term3)))^(1/(2*order + 1))
+
+  band_constant * N^(1 / (2*order + 1))
+}
+
