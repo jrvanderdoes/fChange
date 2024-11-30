d34549f3cf41ac4e948aa5d753996a3504aa5929
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-11-29 21:39:53 -0500

    update general

--- R/kpss_test.R
+++ R/kpss_test.R
@@ -1,14 +1,19 @@
 #' Functional KPSS Test
 #'
-#' @param X description
-#' @param method description
-#' @param boot_method description
-#' @param M description
-#' @param h description
-#' @param TVE description
-#' @param replace description
+#' Compute the KPSS statistic for functional data.
 #'
-#' @return List with statistic and p-value, respectively
+#' @param X Funts object for data
+#' @param method String (default MC) for the method: monte carlo simulation
+#'  (\code{MC}), block permutation (\code{block}), and sliding window
+#'  permutation (\code{sliding})
+#' @param M Number of simulations (default 1000) to estimate theoretical distribution
+#' @param h Numeric (default 3) for the block size when using a permutation test
+#' @param TVE Numeric (default 1) for selecting the number of principle
+#'  components / eigenvalues
+#' @param replace Boolean (default TRUE) to indicate if blocks should be
+#'  selected with replacement when using a permutation test
+#'
+#' @return List with statistic, p-value and theoretical simulations, respectively
 #' @export
 #'
 #' @references Chen, Y., & Pun, C. S. (2019). A bootstrap-based KPSS test for
@@ -17,14 +22,15 @@
 #'  series. Statistics, 50(5), 957-973.
 #'
 #' @examples
-#' compute_kpss(generate_brownian_motion(100,v=seq(0,1,length.out=20)))
-#' compute_kpss(generate_brownian_motion(100,v=seq(0,1,length.out=20)),
-#'              method="BS")
-#' compute_kpss(generate_brownian_motion(100,v=seq(0,1,length.out=20)),
-#'              method="BS", boot_method='overlapping')
-compute_kpss <-function(X, method="MC", boot_method='seperate',
-                        M=1000, h = 3, TVE=0.95, replace=TRUE){
-  X <- .check_data(X)
+#' kpss_test(generate_brownian_motion(100,v=seq(0,1,length.out=20)))
+#' kpss_test(generate_brownian_motion(100,v=seq(0,1,length.out=20)),
+#'              method="block")
+#' kpss_test(generate_brownian_motion(100,v=seq(0,1,length.out=20)),
+#'              method='sliding')
+kpss_test <-function(X, method=c("MC",'block','sliding'),
+                        M=1000, h = 3, TVE=1, replace=TRUE){
+  X <- funts(X)
+  method <- .verify_input(method, c("MC",'block','sliding'))
 
   N <- ncol(X$data)
   r <- nrow(X$data)
@@ -35,21 +41,25 @@ compute_kpss <-function(X, method="MC", boot_method='seperate',
 
   if (method=="MC"){
 
-    pca_X <- pca(X, TVE=TVE)
+    # pca_X <- pca(X, TVE=1)$sdev^2
+    # eigs <- est_eigenvalue(t(hat_eta), K, 2/5)
+    eigs <- pca(funts(hat_eta), TVE=TVE)$sdev^2
 
     sim_RNs <- sapply(1:M,function(m,eigs,v){
       sum(eigs *
-            dot_integrate_col(.generate_second_BB(length(eigs), v = v)$data^2) )
-    },eigs=pca_X$sdev^2, v=X$intraobs)
+            dot_integrate_col(
+              .generate_second_level_brownian_bridge(length(eigs), v = v)$data^2) )
+    },eigs=eigs, v=X$intraobs)
 
-  } else if(method=='BS'){
+  } else if(method=='block' || method=='sliding'){
     xi_hat <- (12/(N*(N^2-1))) *
       rowSums(matrix(rep(1:N-(N+1)/2, r ),
              nrow=r, ncol=N, byrow = TRUE ) * X$data)
     mu_hat <- rowMeans(X$data) - xi_hat * ((N+1)/2)
 
-    boot_etas <- .bootstrap(hat_eta, blockSize=h, M=M,
-                           type=boot_method, replace=replace)
+    boot_etas <- .bootstrap(hat_eta, blocksize=h, M=M,
+                           type=ifelse(method=='block','separate','overlapping'),
+                           replace=replace)
     boot_X <- list()
     sim_RNs <- rep(NA, M)
     for(i in 1:M){
@@ -62,14 +72,14 @@ compute_kpss <-function(X, method="MC", boot_method='seperate',
 
   }
 
-  list(statistic = RN, pvalue = sum(RN <= sim_RNs)/M)
+  list(statistic = RN, pvalue = sum(RN <= sim_RNs)/M, sims=sim_RNs)
 }
 
 
 
 #' Compute KPSS Test statistic
 #'
-#' @inheritParams compute_kpss
+#' @inheritParams kpss_test
 #'
 #' @return List with test statistics and non-integrated values
 #'
@@ -85,30 +95,66 @@ compute_kpss <-function(X, method="MC", boot_method='seperate',
     t( ( (1:N-(N+1)/2) * 6/(N-1) - 1 )  %*% t(mean(X))) -
     t( ( 12/(N*(N^2-1))*(1:N-(N+1)/2) ) %*% t(iX) )
 
-  ZN <- 1/sqrt(N) * cumsum(funts(hat_eta))$data
-  # TODO: UNeven integregrate
-  RN <- dot_integrate(dot_integrate_col(ZN^2)) * r
+  # ZN <- 1/sqrt(N) * cumsum(funts(hat_eta))$data
+  ZN <- .kpss_partial_sum(hat_eta)
+  RN <- dot_integrate(dot_integrate_col(ZN^2, X$intraobs))# * r
 
   list('test_statistic'=RN,'hat_eta'=hat_eta)
 }
 
-#' Generate Second-Order Brownian Motion
+
+#' KPSS Partial Sum Function
 #'
-#' Generates a second-order Brownian Motion for use in KPSS test
+#' Function for partial sum in KPSS test
 #'
-#' @param M Numeric number of simulations
-#' @param v Observation points
+#' @inheritParams kpss_test
 #'
-#' @return funts object of the Brownian Motion
+#' @return Data.frame of eta_hat
 #'
 #' @keywords internal
 #' @noRd
-.generate_second_BB <- function(M,v){
-  # TODO:: Add to generation script
-  W <- generate_brownian_motion(M,v=v)
-  V <- W$data +
-    (2*v-3*v^2) %*% t(W$data[nrow(W$data),]) +
-    (-6*v + 6*v^2) %*% t(dot_integrate_col(W$data))
+.kpss_partial_sum <- function(X){
+  tX <- t(X)
+  N <- nrow(tX)
+  res <- ncol(tX)
+  teta <- matrix(rep(0,res*res),ncol=res)
 
-  funts(X=V,intraobs = v)
+  if (floor(N/res)>0) {
+    for (i in 1:floor(N/res)){
+      teta[1,] <- teta[1,] + tX[i,]/sqrt(N)
+    }
+  }
+
+  if (res>1){
+    for (i in 2:res){
+      teta[i,] <- teta[i-1,]
+      if (floor(N*(i-1)/res) < floor(N*i/res)) {
+        for (j in (floor(N*(i-1)/res)+1):floor(N*i/res)){
+          teta[i,] <- teta[i,] + tX[j,]/sqrt(N)
+        }
+      }
+    }
+  }
+
+  t(teta)
 }
+
+
+# est_eigenvalue<-function(este,K,h_power=2/5){
+#   N <- nrow(este)
+#   res <- ncol(este)
+#
+#   gamma<-array(0, dim=c(N,res,res))
+#   for (i in 1:N){
+#     for (j in i:N){
+#       gamma[i,,] <- gamma[i,,] + ( este[j,] %*% t(este[j-i+1,]) )/N
+#     }
+#   }
+#
+#   cc <- gamma[1,,]
+#   for (i in 2:N){
+#     cc <- cc + K( (i-1)/(N^(h_power)) ) * (gamma[i,,]+t(gamma[i,,]))
+#   }
+#
+#   eigen(cc/res)$values
+# }
