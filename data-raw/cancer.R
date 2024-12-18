f20764f37732afb0beec68594fde5c1a5345b582
jrvanderdoes <jeremyvanderdoes@gmail.com>
2024-12-17 21:43:58 -0500

    update data

---
+++ data-raw/cancer.R
@@ -0,0 +1,14 @@
+# USA Breast Cancer Data from WHO
+load(file = "./data-raw/cancer.RData")
+
+cancer <- cancer %>%
+  tidyr::pivot_wider(id_cols = `Age Group`, names_from = Year,
+                     values_from = `Percentage of cause-specific deaths out of total deaths`)
+cnames <- colnames(cancer)
+
+cancer <-  cancer %>% data.frame()
+rownames(cancer) <- cancer[,1]
+cancer <- cancer[,-1]
+colnames(cancer) <- cnames[-1]
+
+use_data(cancer)
