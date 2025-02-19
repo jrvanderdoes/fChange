d1e32ed2653f27983a4c414e2ea6c134873a69fc
jrvanderdoes <jeremyvanderdoes@gmail.com>
2025-02-18 23:43:42 -0500

    Update raw data files

--- data-raw/electricity.R
+++ data-raw/electricity.R
@@ -1,7 +1,12 @@
 load(file = "./data-raw/electricity.RData")
-colnames(electricity) <-
-  as.character(seq(from = as.Date('01-01-2014','%m-%d-%Y'),
-      to = as.Date('12-31-2014','%m-%d-%Y'),
-      by = "days"))
+# colnames(electricity) <-
+#   as.character(seq(from = as.Date('01-01-2014','%m-%d-%Y'),
+#       to = as.Date('12-31-2014','%m-%d-%Y'),
+#       by = "days"))
+
+electricity <- dfts(electricity,name='Spanish Electricity Spot Prices 2014',
+                    labels = as.character(seq(from = as.Date('01-01-2014','%m-%d-%Y'),
+                                              to = as.Date('12-31-2014','%m-%d-%Y'),
+                                              by = "days")))
 
 use_data(electricity)
