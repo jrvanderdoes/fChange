027ea70514f93ca88124a8616bc4d73988736d63
jrvanderdoes <jeremyvanderdoes@gmail.com>
2025-06-20 16:34:02 -0400

    Workshop and other documentation

--- R/ZZ_forecasting_model.R
+++ R/ZZ_forecasting_model.R
@@ -6,17 +6,18 @@
 #'
 #' @noRd
 #' @keywords internal
-competition_error <- function(estimate){
+competition_error <- function(estimate) {
+  urlfile <- paste0(
+    "https://raw.githubusercontent.com/jrvanderdoes/fChange",
+    "/main/vignettes/articles/functionaldata/practice.csv"
+  )
+  data_true <- utils::read.csv(urlfile)[, -1]
 
-  urlfile <-  paste0('https://raw.githubusercontent.com/jrvanderdoes/fChange',
-                     '/main/vignettes/articles/functionaldata/practice.csv')
-  data_true <- utils::read.csv(urlfile)[,-1]
-
-  if(is.dfts(estimate)) estimate <- estimate$data
+  if (is.dfts(estimate)) estimate <- estimate$data
 
   error <- rep(NA, 14)
-  for(i in 1:14){
-    error[i] <- sum((estimate[,i] - data_true[,i])^2)
+  for (i in 1:14) {
+    error[i] <- sum((estimate[, i] - data_true[, i])^2)
   }
 
   mean(error)
